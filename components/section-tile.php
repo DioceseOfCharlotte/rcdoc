e421be9e5d5d9d660fd8f7bffeca0cda577fc5de
Marty Helmick <info@martyhelmick.com>
2015-10-12 22:25:53 -0400

    add tile template

---
+++ components/section-tile.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * List of related articles.
+ *
+ * @package abraham
+ */
+?>
+<section class="tile mdl-cell mdl-card mdl-shadow--2dp">
+            <a href="<?php the_permalink(); ?>" class="u-block">
+            <?php get_template_part('images/icon', 'links'); ?>
+            </a>
+</section>
