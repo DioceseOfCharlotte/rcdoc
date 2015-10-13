2e871a5ee2a571bafc041e252b91b3b1bfaf6bd8
m-e-h <info@martyhelmick.com>
2015-10-13 03:03:57 -0400

    tiles

--- components/section-tile.php
+++ components/section-tile.php
@@ -4,9 +4,13 @@
  *
  * @package abraham
  */
+ global $mehsc_atts;
 ?>
-<section class="tile mdl-cell mdl-card mdl-shadow--2dp">
-            <a href="<?php the_permalink(); ?>" class="u-block">
-            <?php get_template_part('images/icon', 'links'); ?>
+<section id="post-<?php the_ID(); ?>" class="tile mdl-cell mdl-card mdl-shadow--2dp">
+            <a href="<?php the_permalink(); ?>" class="mdl-card__title mdl-card--expand u-flex-column">
+            <?php get_template_part('images/icon', get_the_slug() ); ?>
+            <div class="mdl-card__actions">
+                <h4><?php the_title(); ?></h4>
+            </div>
             </a>
 </section>
