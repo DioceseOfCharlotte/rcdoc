cc68767ac08e3f4f0f7164e547e4c753defc5c55
Marty Helmick <info@martyhelmick.com>
2015-10-13 22:59:18 -0400

    add color class

--- components/section-tile.php
+++ components/section-tile.php
@@ -6,7 +6,7 @@
  */
  global $mehsc_atts;
 ?>
-            <div id="post-<?php the_ID(); ?>" class="tile mdl-cell mdl-card mdl-shadow--2dp">
+            <div id="post-<?php the_ID(); ?>" class="tile <?php the_field('accent_color'); ?> mdl-cell mdl-card mdl-shadow--2dp">
                 <a href="<?php the_permalink(); ?>" class="mdl-card__title mdl-card--expand u-flex-column u-flex-justify-center u-text-center">
                     <?php get_template_part('images/icon', get_the_slug() ); ?>
                     <div class="mdl-card__actions">
