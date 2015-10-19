62f11c74391351ad558348896aef7b1d0b580663
m-e-h <info@martyhelmick.com>
2015-10-19 13:28:48 -0400

    only show footer on archive

--- components/entry-footer.php
+++ components/entry-footer.php
@@ -1,3 +1,7 @@
+<?php if ( !is_archive() ) {
+    return;
+}
+?>
 <footer <?php hybrid_attr('entry-footer'); ?>>
     <a href="<?php the_permalink(); ?>" class="mdl-button mdl-js-button mdl-js-ripple-effect"><?php esc_html_e( 'More', 'abraham' ); ?></a>
     <?php get_template_part('components/child', 'links'); ?>
