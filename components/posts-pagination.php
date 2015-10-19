ae6df21bcd3ad22bc966fe098622c06c731ae9c2
m-e-h <info@martyhelmick.com>
2015-10-19 11:58:02 -0400

    color nav links

--- components/posts-pagination.php
+++ components/posts-pagination.php
@@ -1,10 +1,10 @@
-<div class="u-1/1 u-text-center u-mb3 u-p2 u-bg-white mdl-shadow--2dp u-br">
+<div class="u-1/1 u-text-center u-mb3 u-p2 u-bg-white u-color-inherit mdl-shadow--2dp u-br">
 
     <?php
     the_posts_pagination( array(
         'next_text' =>  '<span class="meta-nav mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect mdl-button" aria-hidden="true"><i class="material-icons">&#xE409;</i></span> ',
         'prev_text' => '<span class="meta-nav mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect mdl-button" aria-hidden="true"><i class="material-icons">&#xE408;</i></span> ',
-        'before_page_number' => '<span class="u-text-white meta-nav screen-reader-text">' . __( 'Page', 'abraham' ) . ' </span>',
+        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'abraham' ) . ' </span>',
     ) );
     ?>
 
