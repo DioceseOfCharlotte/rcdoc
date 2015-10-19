bf3bc883812535f8a2617a07fab797e4be995a52
m-e-h <info@martyhelmick.com>
2015-10-19 13:47:42 -0400

    drop the footer for now

--- components/entry-footer.php
+++ components/entry-footer.php
@@ -1,6 +1,6 @@
-<?php if ( !is_archive() ) {
-    return;
-}
+<?php // if ( !is_archive() ) {
+    return
+//}
 ?>
 <footer <?php hybrid_attr('entry-footer'); ?>>
     <a href="<?php the_permalink(); ?>" class="mdl-button mdl-js-button mdl-js-ripple-effect"><?php esc_html_e( 'More', 'abraham' ); ?></a>
