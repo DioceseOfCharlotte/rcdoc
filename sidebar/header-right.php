1dd3f947aff9c3c49f4807ca3cd837108f902c05
m-e-h <info@martyhelmick.com>
2015-10-24 17:16:53 -0400

    style the widget dropdown

--- sidebar/header-right.php
+++ sidebar/header-right.php
@@ -11,6 +11,6 @@ if (!is_active_sidebar('header-right')) {
 </form>
 
     <button href="#" class="js-drop-panel dropdown-btn material-icons u-absolute u-p1/2 u-text-white u-round u-top0 u-z4 u-m2 u-right0">account_circle</button>
-    <div class="dropdown-menu-basic dropdown-right u-text-black">
+    <div class="dropdown-menu-basic dropdown-right u-br u-p1 u-bg-white u-mr3 mdl-shadow--3dp u-text-black">
         <?php dynamic_sidebar('header-right'); ?>
     </div>
