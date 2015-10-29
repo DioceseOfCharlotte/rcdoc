14fc776d66430b8ae14e16740582ed0feade8b6a
m-e-h <info@martyhelmick.com>
2015-10-29 10:28:37 -0400

    no flex

--- inc/html-classes.php
+++ inc/html-classes.php
@@ -35,7 +35,7 @@ function rcdoc_html_classes() {
 
 		'entry_title'             	=> 'mdl-card__title-text',
 		'page_title'    		  	=> 'u-display-2 u-m0 u-py3',
-		'archive_description'     	=> 'u-1/1 u-px3 u-pb0 u-pt3 u-mb1 u-mx1 u-br u-flex u-flex-justify u-bg-frost-4 mdl-shadow--3dp',
+		'archive_description'     	=> 'u-1/1 u-px3 u-pb0 u-pt3 u-mb1 u-mx1 u-br u-bg-frost-4 mdl-shadow--3dp',
 
 		'entry_header'            	=> 'mdl-card__title u-pt0 u-px0',
 		'entry_content'           	=> 'u-px2 u-pb2',
