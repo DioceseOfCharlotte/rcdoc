da434bf7340ca6a4d19f722318b73226b4ed3cac
Marty Helmick <info@martyhelmick.com>
2015-11-06 18:53:34 -0500

    more visible search input

--- sidebar/header-right.php
+++ sidebar/header-right.php
@@ -6,7 +6,7 @@ if (!is_active_sidebar('header-right')) {
 <form action="/" method="get" class="mdl-textfield mdl-js-textfield u-absolute u-right0 u-mr4 u-z4 u-text-white mdl-textfield--expandable u-ml-auto" action="<?php echo home_url( '/' ); ?>">
 	<label class="mdl-button mdl-js-button mdl-button--icon u-m0" for="search"><i class="material-icons">search</i></label>
 <div class="mdl-textfield__expandable-holder">
-	<input class="mdl-textfield__input u-lh-2 search-field u-p0 u-border0 u-text-white u-bg-frost-1" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+	<input class="mdl-textfield__input u-lh-2 search-field u-p0 u-border0 u-text-white u-bg-frost-2" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
 </div>
 </form>
 
