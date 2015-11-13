3a0aa21c531069ff626a2a03ec27bf12b12d92a1
m-e-h <info@martyhelmick.com>
2015-11-13 09:47:57 -0500

    change color variables

--- components/row-tabs.php
+++ components/row-tabs.php
@@ -13,24 +13,24 @@ $query = new WP_Query( $args );
 
 if ( $query->have_posts() ) { ?>
 
-<div class="mdl-cell row__tabs mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+<div class="mdl-cell row__tabs u-text-white u-currentcolor_a mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
   <div class="mdl-tabs__tab-bar">
-    
+
     <?php while ($query->have_posts()) : $query->the_post(); ?>
 
       <a href="#tab<?php the_ID(); ?>" class="mdl-tabs__tab"><?php the_title(); ?></a>
 
     <?php endwhile; ?>
-    
+
   </div>
-    
+
   <?php while ($query->have_posts()) : $query->the_post(); ?>
-  
-  <div class="mdl-tabs__panel" id="tab<?php the_ID(); ?>">
+
+  <div class="mdl-tabs__panel u-p2" id="tab<?php the_ID(); ?>">
     <?php the_content(); ?>
   </div>
 
   <?php endwhile; ?>
-    
+
 </div>
 <?php }
