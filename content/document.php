300c9fbe4e19ebbdb3b982b635e4ad3ce86062ff
m-e-h <info@martyhelmick.com>
2016-01-24 18:29:46 -0500

    remove unused markup

--- content/document.php
+++ content/document.php
@@ -1,21 +1,17 @@
 
 
-<article <?php hybrid_attr('post'); ?> class="u-flexed-start">
+<div <?php hybrid_attr('post'); ?>>
 
 	<?php tha_entry_top(); ?>
 
-		<header <?php hybrid_attr('entry-header'); ?>>
-			<div <?php hybrid_attr('entry-title'); ?>>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</div>
-		</header>
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
-		<?php tha_entry_content_before(); ?>
-		<?php the_excerpt(); ?>
-		<?php tha_entry_content_after(); ?>
+		<div class="doc-excerpt">
+			<?php tha_entry_content_before(); ?>
+			<?php the_excerpt(); ?>
+			<?php tha_entry_content_after(); ?>
+		</div>
 
-		<?php get_template_part('components/entry', 'footer'); ?>
+	<?php tha_entry_bottom(); ?>
 
-<?php tha_entry_bottom(); ?>
-
-</article>
+</div>
