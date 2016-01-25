83b31b2738160f0044e0c910873486b2a6a1d1db
m-e-h <info@martyhelmick.com>
2016-01-25 13:57:01 -0500

    show all contact info

--- components/acf-parish-contact.php
+++ components/acf-parish-contact.php
@@ -13,14 +13,28 @@ $address = get_field('doc_street') . "+" . get_field('doc_city') . "+" . get_fie
 
 $map_link = "http://maps.google.com/maps?z=16&q=" . $address;
 
+?>
 
-
-
+<div class="u-flex contact-info u-flex-wrap u-flex-justify-between u-mxn1">
+<?php
 if( get_field('doc_city') ):
 ob_start();
 ?>
-<p class="card-address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-    <a class="contact-link" href="<?php echo esc_url( $map_link ) ?>" target="_blank"><i class="material-icons u-align-top map-marker">&#xE55F;</i>
+<div class="contact-numbers u-overflow-hidden u-px2 u-inline-block">
+<?php if( get_field('doc_phone_number') ): ?>
+    <div class="phone u-mb1" itemprop="telephone"><a class="contact-link" href="tel:<?php the_field('doc_phone_number'); ?>"><i class="material-icons u-mr1">&#xE0CD;</i><?php the_field('doc_phone_number'); ?></a></div>
+<?php endif; ?>
+<?php if( get_field('doc_fax') ): ?>
+    <div class="fax u-mb1" itemprop="faxNumber"><i class="u-bold u-mr1">FAX</i><?php the_field('doc_fax'); ?></div>
+<?php endif; ?>
+</div>
+
+<?php
+echo ob_get_clean();
+endif;
+ob_start(); ?>
+<div class="contact-address u-px2 u-inline-block u-mb1" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+    <a class="contact-link" href="<?php echo esc_url( $map_link ) ?>" target="_blank"><i class="material-icons u-align-top map-marker u-mr1">&#xE55F;</i>
     <span class="u-inline-block">
         <span itemprop="streetAddress">
             <?php the_field('doc_street'); ?><br>
@@ -31,27 +45,15 @@ ob_start();
         <span itemprop="addressLocality"><?php the_field('doc_city'); ?>, <?php the_field('doc_state'); ?></span>
         <span itemprop="postalCode"><?php the_field('doc_zip'); ?></span>
     </span>
-</a></p>
-    <?php if( get_field('doc_website') ): ?>
-        <p class="website-address" itemprop="url"><a class="contact-link" href="<?php the_field('doc_website'); ?>"><i class="u-align-middle material-icons">&#xE80B;</i> <?php the_field('doc_website'); ?></a></p>
-    <?php endif; ?>
+</a>
+</div>
 <?php
 echo ob_get_clean();
-endif;
-ob_start(); ?>
-<div class="contact-methods u-overflow-hidden u-px1">
-<p class="contact-numbers">
-<?php if( get_field('doc_phone_number') ): ?>
-    <p class="phone" itemprop="telephone"><a class="contact-link" href="tel:<?php the_field('doc_phone_number'); ?>"><i class="u-align-middle material-icons">&#xE0CD;</i> <?php the_field('doc_phone_number'); ?></a></p>
-<?php endif; ?>
-<?php if( get_field('doc_fax') ): ?>
-    <p class="fax" itemprop="faxNumber"><i class="u-bold u-align-middle">FAX</i> <?php the_field('doc_fax'); ?></p>
-<?php endif; ?>
-</p>
+?>
 <?php if( get_field('doc_email') ): ?>
-    <p class="email" itemprop="email"><a class="contact-link" href="mailto:<?php the_field('doc_email'); ?>"><i class="u-align-middle material-icons">&#xE0BE;</i> <?php the_field('doc_email'); ?></a></p>
+    <div class="email u-1of1 u-mb1 u-px2" itemprop="email"><a class="contact-link" href="mailto:<?php the_field('doc_email'); ?>"><i class="material-icons u-mr1">&#xE0BE;</i><?php the_field('doc_email'); ?></a></div>
+<?php endif; ?>
+<?php if( get_field('doc_website') ): ?>
+    <div class="website-address u-bg-tint-1 u-1of1 u-text-center" itemprop="url"><a class="contact-link u-1of1 btn u-h6" href="<?php the_field('doc_website'); ?>" target="_blank"><?php the_field('doc_website'); ?><i class="u-px1 u-h6 material-icons">&#xE89E;</i></a></div>
 <?php endif; ?>
 </div>
-  <button class="u-flexed-center u-1of1 js-dropdown js-with-parent js-drop-before material-icons">&#xE5C5;</button>
-<?php
-echo ob_get_clean();
