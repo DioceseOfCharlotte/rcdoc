0134004c13af243e5315d87010e6f501a6be9e60
m-e-h <info@martyhelmick.com>
2016-01-25 16:06:44 -0500

    use new dynamic text color

--- components/row-feed.php
+++ components/row-feed.php
@@ -1,8 +1,8 @@
-<div class="o-cell u-1of2-md u-text-1-dark u-flex u-flex-justify-center u-flex-center">
+<div class="o-cell u-1of2-md u-flex u-flex-justify-center u-flex-center">
     <?php get_template_part('assets/images/icon', esc_attr( $attr['icon_file'] ) ); ?>
 </div>
 
-<div class="o-cell u-1of2-md u-text-white u-flex u-flex-justify-center u-flex-center">
+<div class="o-cell u-1of2-md u-flex u-flex-justify-center u-flex-center">
 <?php
 the_widget( 'WP_Widget_RSS',
     array(
@@ -12,7 +12,7 @@ the_widget( 'WP_Widget_RSS',
         //'show_summary' => true,
     ),
     array(
-        'before_widget' => '<section class="rss-widget u-currentcolor_a o-cell u-1of1 mdl-mega-footer__drop-down-section u-p2 u-flexed-grow"><div>',
+        'before_widget' => '<section class="rss-widget o-cell u-1of1 mdl-mega-footer__drop-down-section u-p2 u-flexed-grow"><div>',
         'after_widget'  => '</div></section>',
         'before_title'  => '</div><input class="mdl-mega-footer__heading-checkbox u-1of1" type="checkbox" checked><h2 class="widget-title u-h1 u-mt0 mdl-mega-footer--heading rss-title">',
         'after_title'   => '</h2><div class="mdl-mega-footer--link-list u-list-reset">',
