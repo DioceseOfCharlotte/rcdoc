6a7b6fc618c3ba249a77ec8ca0d47318c25b0be5
m-e-h <info@martyhelmick.com>
2016-01-26 15:35:43 -0500

    show markup even if empty

--- components/acf-parish-contact.php
+++ components/acf-parish-contact.php
@@ -21,39 +21,57 @@ $map_link = "http://maps.google.com/maps?z=16&q=" . $address;
 ob_start();
 ?>
 <div class="contact-numbers u-overflow-hidden u-px2 u-inline-block">
+
+    <div class="phone u-mb1">
 <?php if( get_field('doc_phone_number') ): ?>
-    <div class="phone u-mb1" itemprop="telephone"><a class="contact-link" href="tel:<?php the_field('doc_phone_number'); ?>"><i class="material-icons u-mr1">&#xE0CD;</i><?php the_field('doc_phone_number'); ?></a></div>
+        <a class="contact-link" href="tel:<?php the_field('doc_phone_number'); ?>" itemprop="telephone"><i class="material-icons u-mr1">&#xE0CD;</i><?php the_field('doc_phone_number'); ?></a>
 <?php endif; ?>
+    </div>
+
+
+    <div class="fax u-mb1">
 <?php if( get_field('doc_fax') ): ?>
-    <div class="fax u-mb1" itemprop="faxNumber"><i class="u-bold u-mr1">FAX</i><?php the_field('doc_fax'); ?></div>
+        <span itemprop="faxNumber"><i class="u-bold u-mr1">FAX</i><?php the_field('doc_fax'); ?></span>
 <?php endif; ?>
+    </div>
+
 </div>
 
 <?php
 echo ob_get_clean();
-if( get_field('doc_city') ):
+
 ob_start(); ?>
-<div class="contact-address u-px2 u-inline-block u-mb1" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-    <a class="contact-link" href="<?php echo esc_url( $map_link ) ?>" target="_blank"><i class="material-icons u-align-top map-marker u-mr1">&#xE55F;</i>
-    <span class="u-inline-block">
-        <span itemprop="streetAddress">
-            <?php the_field('doc_street'); ?><br>
-            <?php if( get_field('doc_street_2') ) { ?>
-            <?php the_field('doc_street_2'); ?><br>
-            <?php } ?>
+<div class="contact-address u-px2 u-inline-block u-mb1">
+<?php if( get_field('doc_city') ): ?>
+    <a class="contact-link" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress" href="<?php echo esc_url( $map_link ) ?>" target="_blank"><i class="material-icons u-align-top map-marker u-mr1">&#xE55F;</i>
+        <span class="u-inline-block">
+            <span itemprop="streetAddress">
+                <?php the_field('doc_street'); ?><br>
+                <?php if( get_field('doc_street_2') ) { ?>
+                <?php the_field('doc_street_2'); ?><br>
+                <?php } ?>
+            </span>
+            <span itemprop="addressLocality"><?php the_field('doc_city'); ?>, <?php the_field('doc_state'); ?></span>
+            <span itemprop="postalCode"><?php the_field('doc_zip'); ?></span>
         </span>
-        <span itemprop="addressLocality"><?php the_field('doc_city'); ?>, <?php the_field('doc_state'); ?></span>
-        <span itemprop="postalCode"><?php the_field('doc_zip'); ?></span>
-    </span>
-</a>
+    </a>
+<?php endif; ?>
 </div>
 <?php echo ob_get_clean(); ?>
-<?php endif; ?>
 
+
+
+    <div class="email u-1of1 u-mb1 u-px2">
 <?php if( get_field('doc_email') ): ?>
-    <div class="email u-1of1 u-mb1 u-px2" itemprop="email"><a class="contact-link" href="mailto:<?php the_field('doc_email'); ?>"><i class="material-icons u-mr1">&#xE0BE;</i><?php the_field('doc_email'); ?></a></div>
+        <a class="contact-link" itemprop="email" href="mailto:<?php the_field('doc_email'); ?>"><i class="material-icons u-mr1">&#xE0BE;</i><?php the_field('doc_email'); ?></a>
 <?php endif; ?>
+    </div>
+
+
+    <div class="website-address u-bg-2-glass u-1of1 u-text-center">
 <?php if( get_field('doc_website') ): ?>
-    <div class="website-address u-bg-2-glass u-1of1 u-text-center" itemprop="url"><a class="contact-link u-1of1 btn u-h6" href="<?php the_field('doc_website'); ?>" target="_blank"><?php the_field('doc_website'); ?><i class="u-px1 u-h6 material-icons">&#xE89E;</i></a></div>
+        <a class="contact-link u-1of1 btn u-h6" itemprop="url" href="<?php the_field('doc_website'); ?>" target="_blank"><?php the_field('doc_website'); ?><i class="u-px1 u-h6 material-icons">&#xE89E;</i></a>
 <?php endif; ?>
+    </div>
+
 </div>
