05bd1ebab17d60c1d60fb990f0ac06f99421f8a7
m-e-h <info@martyhelmick.com>
2016-01-29 19:16:59 -0500

    styles

--- inc/ext/facetwp.php
+++ inc/ext/facetwp.php
@@ -81,20 +81,20 @@ function doc_display_facets() {
 		echo facetwp_display( 'facet', 'department_search' );
         echo facetwp_display( 'facet', 'document_categories' );
         echo facetwp_display( 'facet', 'document_types' );
-        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
+        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab u-m0 mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
         echo '</div>';
     } elseif ( is_post_type_archive('parish') ) {
 		echo '<div class="u-1of1 u-px3 u-pb0 u-br u-pt3 u-mb1 u-mx1 u-flex u-flex-wrap u-flex-justify-around u-bg-frost-4">';
         echo facetwp_display( 'facet', 'parish_proximity' );
 		echo facetwp_display( 'facet', 'department_search' );
-        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
+        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab u-m0 mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
         echo '<div class="u-1of1 u-text-center">' .facetwp_display( 'facet', 'title_alpha' ). '</div>';
         echo '</div>';
     } elseif ( is_post_type_archive('department') ) {
 		echo '<div class="u-1of1 u-px3 u-pb0 u-br u-pt3 u-mb1 u-mx1 u-flex u-flex-wrap u-flex-justify-around u-bg-frost-4">';
         echo facetwp_display( 'facet', 'department_agency' );
 	    echo facetwp_display( 'facet', 'department_search' );
-        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
+        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab u-m0 mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
         echo '<div class="u-1of1 u-text-center">' .facetwp_display( 'facet', 'title_alpha' ). '</div>';
 		echo '</div>';
     } elseif ( is_post_type_archive('school') ) {
@@ -102,7 +102,7 @@ function doc_display_facets() {
 		echo facetwp_display( 'facet', 'parish_proximity' );
 	    echo facetwp_display( 'facet', 'school_system' );
 		echo facetwp_display( 'facet', 'department_search' );
-        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
+        echo '<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab u-m0 mdl-js-ripple-effect u-m0" onclick="FWP.reset()"><i class="material-icons">&#xE5D5;</i></button>';
 		echo '<div class="u-1of1 u-text-center">' .facetwp_display( 'facet', 'title_alpha' ). '</div>';
 		echo '</div>';
     }
