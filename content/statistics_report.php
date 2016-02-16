82fd314fd125ab82277e7c32d385da0c6a889d53
m-e-h <info@martyhelmick.com>
2016-02-16 15:31:43 -0500

    statistics template

--- content/statistics_report.php
+++ content/statistics_report.php
@@ -1,36 +1,17 @@
 <?php
 /**
- * General fallback template for post archives.
+ * Template for statistics.
  *
  * @package abraham
  */
 ?>
-<article <?php hybrid_attr('post'); ?>>
+<?php $url = get_post_meta( get_the_ID(), 'doc_stats_report', true ); ?>
+<div <?php hybrid_attr('post'); ?>>
 
 	<?php tha_entry_top(); ?>
 
-		<header <?php hybrid_attr('entry-header'); ?>>
-			<?php
-				get_the_image(array(
-					'size' => 'abe-card-md',
-					'image_class' => 'u-br-t u-1of1',
-					'before'             => '<div class="card-img u-overflow-hidden">',
-					'after'              => '</div>',
-				));
-			?>
-			<h2 <?php hybrid_attr('entry-title'); ?>>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</h2>
-		</header>
+		<a class="btn btn-hollow u-1of1" href="<?= esc_url($url); ?>"><i class="material-icons">&#xE24D;</i> <?php the_title(); ?><i class="material-icons u-text-white u-right">&#xE895;</i></a>
 
-		<div <?php hybrid_attr('entry-summary'); ?>>
-			<?php tha_entry_content_before(); ?>
-			<?php abe_excerpt(); ?>
-			<?php tha_entry_content_after(); ?>
-		</div>
+	<?php tha_entry_bottom(); ?>
 
-		<?php get_template_part('components/entry', 'footer'); ?>
-
-<?php tha_entry_bottom(); ?>
-
-</article>
+</div>
