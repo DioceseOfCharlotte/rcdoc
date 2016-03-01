8b36bac1a6bf5de88e42952b94e84239683623ca
Marty Helmick <info@martyhelmick.com>
2016-02-29 19:47:27 -0500

    rename box-shadow utility class

--- sidebar/header-right.php
+++ sidebar/header-right.php
@@ -4,7 +4,7 @@ if (!is_active_sidebar('header-right')) {
 } ?>
 <div class="u-inline-block sidebar-header u-rel">
     <button href="#" class="js-dropdown dropdown-btn btn-round material-icons u-z4">account_circle</button>
-    <div class="dropdown-menu-basic dropdown-right u-br u-px1 u-right0 u-bg-white mdl-shadow--3dp u-text-color">
+    <div class="dropdown-menu-basic dropdown-right u-br u-px1 u-right0 u-bg-white u-shadow--3dp u-text-color">
         <?php dynamic_sidebar('header-right'); ?>
     </div>
 </div>
