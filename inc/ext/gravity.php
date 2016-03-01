f450dd1bd66e76df74f76c17473bf6018d2ec927
m-e-h <info@martyhelmick.com>
2016-03-01 10:41:11 -0600

    compile

--- inc/ext/gravity.php
+++ inc/ext/gravity.php
@@ -1,3 +1,4 @@
 <?php
 add_post_type_support('gravityview', 'theme-layouts');
 add_filter('gravityview/widget/enable_custom_class', '__return_true' );
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
