0cd02f5ad856cf95b854adcaccac1f80340d16fa
m-e-h <info@martyhelmick.com>
2016-03-30 14:43:39 -0400

    crop image. use smaller text

--- components/row-slides.php
+++ components/row-slides.php
@@ -6,25 +6,23 @@
  while ($query->have_posts()) : $query->the_post();
 ?>
 
-<div class ="gallery-cell">
-<div id="post-<?php the_ID(); ?>" class="o-cell u-bg-white u-br u-shadow--3dp">
+<div id="post-<?php the_ID(); ?>" class="gallery-cell o-cell u-bg-white u-br u-shadow--3dp">
 
     <header <?php hybrid_attr('entry-header'); ?>>
         <?php
             get_the_image(array(
                 'size'         => 'abe-hd',
-                //'image_class'  => 'o-crop__content',
+                'image_class'  => 'o-crop__content',
                 'link_to_post' => false,
-                //'before'       => '<div class="o-crop o-crop--16x9">',
-                //'after'        => '</div>',
+                'before'       => '<div class="o-crop o-crop--16x9">',
+                'after'        => '</div>',
             ));
         ?>
-        <h3 class="card-title u-text-center u-m0">
-            <a class="btn btn-hollow u-border0 u-1of1 u-bold" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        <h3 class="card-title u-h4 u-text-center u-1of1 u-m0">
+            <a class="btn btn-hollow u-border0 u-1of1" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </h2>
     </header>
 
-</div>
 </div>
 <?php
 endwhile;
