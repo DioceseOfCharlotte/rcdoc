54875986f0c0e174d4dd14e1ba4ad4f554eb5369
m-e-h <info@martyhelmick.com>
2016-04-01 19:33:53 -0400

    style gravityview links

--- inc/ext/gravity.php
+++ inc/ext/gravity.php
@@ -2,12 +2,16 @@
 add_post_type_support('gravityview', 'theme-layouts');
 add_filter('gravityview/widget/enable_custom_class', '__return_true' );
 add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+add_filter('gravityview/extension/search/links_sep', '__return_false' );
+add_filter('gravityview/extension/search/links_label', '__return_false' );
 
 
 add_filter( 'gform_pre_render_3', 'populate_dept' );
 add_filter( 'gform_pre_validation_3', 'populate_dept' );
 add_filter( 'gform_pre_submission_filter_3', 'populate_dept' );
 add_filter( 'gform_admin_pre_render_3', 'populate_dept' );
+
+
 function populate_dept( $form ) {
 
     foreach ( $form['fields'] as &$field ) {
@@ -33,4 +37,29 @@ function populate_dept( $form ) {
     }
 
     return $form;
+}
+
+
+// Add the filter and pass two arguments ($existing_text and $is_search) to the
+// modify_gravitview_no_entries_text() function
+add_filter( 'gravitview_no_entries_text', 'modify_gravitview_no_entries_text', 10, 2 );
+/**
+ * Modify the text displayed when there are no entries.
+ *
+ * Place this code (after <?php) at the bottom of your theme's functions.php file to enable it
+ *
+ * @param string $existing_text The existing "No Entries" text
+ * @param bool $is_search  Is the current page a search result, or just a multiple entries screen?
+ */
+function modify_gravitview_no_entries_text( $existing_text, $is_search ) {
+
+	$return = $existing_text;
+
+	if( $is_search ) {
+		$return = 'Sorry, but nothing matched your search. Perhaps try again with some different keywords.';
+	} else {
+		$return = "Hello";
+	}
+
+	return $return;
 }
