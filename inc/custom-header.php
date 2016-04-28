0d7a3272653f9a5f37998c18b80e89afca8e26f4
m-e-h <info@martyhelmick.com>
2016-04-28 16:15:05 -0400

    term image

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -51,7 +51,12 @@ function abraham_custom_header_wp_head() {
 	}
 
 	global $cptarchives;
-	if ( has_post_thumbnail( $cptarchives->get_archive_id() ) ) {
+	$queried_object = get_queried_object()->term_id;
+	$term_image = get_term_meta( $queried_object, 'image', true );
+	if ( $term_image ) {
+		$bg_image = wp_get_attachment_image_url( $term_image, 'abe-hd' );
+
+	} elseif ( has_post_thumbnail( $cptarchives->get_archive_id() ) ) {
 		$bg_image = wp_get_attachment_image_url( get_post_thumbnail_id( $cptarchives->get_archive_id() ), 'abe-hd' );
 
 	} elseif ( has_post_thumbnail() ) {
