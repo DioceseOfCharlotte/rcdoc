438bd7c074e65829b05941da0c9f0808eb8303b0
m-e-h <info@martyhelmick.com>
2016-04-28 10:58:12 -0400

    mobile widgets

--- components/row-feed.php
+++ components/row-feed.php
@@ -10,9 +10,9 @@
 <div class="o-cell u-1of2-md u-flex u-flex-jc u-flex-center">
 	<?php if ( locate_template( 'images/icons/' . esc_attr( $attr['icon_file'] ) . '.svg' ) ) {
 		include locate_template( 'images/icons/' . esc_attr( $attr['icon_file'] ) . '.svg' );
-	} else {
-		include locate_template( 'images/icons/shield.svg' );
-	} ?>
+} else {
+	include locate_template( 'images/icons/shield.svg' );
+} ?>
 </div>
 
 <div class="o-cell u-1of2-md u-flex u-flex-jc u-flex-center">
@@ -25,10 +25,10 @@ the_widget( 'WP_Widget_RSS',
 		// 'show_summary' => true,
 	),
 	array(
-		'before_widget' => '<section class="rss-widget o-cell u-1of1 mdl-mega-footer__drop-down-section u-p2 u-flexed-grow"><div>',
+		'before_widget' => '<section class="rss-widget o-cell u-1of1 mobile-widget__drop-down-section u-p2 u-flexed-grow"><div>',
 		'after_widget'  => '</div></section>',
-		'before_title'  => '</div><input class="mdl-mega-footer__heading-checkbox u-1of1" type="checkbox" checked><h2 class="widget-title u-h2 u-mt0 mdl-mega-footer--heading rss-title">',
-		'after_title'   => '</h2><div class="mdl-mega-footer--link-list u-f-plus u-list-reset">',
+		'before_title'  => '</div><input class="mobile-widget__heading-checkbox u-1of1" type="checkbox" checked><h2 class="widget-title u-h2 u-mt0 mobile-widget__heading rss-title">',
+		'after_title'   => '</h2><div class="mobile-widget__link-list u-f-plus u-list-reset">',
 	)
 );
 ?>
