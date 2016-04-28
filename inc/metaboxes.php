0afda9e397a543d38bf6e73b808f3bd6b2b85985
m-e-h <info@martyhelmick.com>
2016-04-28 18:29:11 -0400

    lint

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -22,7 +22,7 @@ function doc_register_dept_alias() {
 	$doc_dept_alias = new_cmb2_box( array(
 		'id'            => $prefix . 'metabox',
 		'title'         => __( 'Dept Alias', 'cmb2' ),
-		'object_types'  => array( 'department', ),
+		'object_types'  => array( 'department' ),
 		'context'       => 'side',
 		'priority'      => 'low',
 	) );
@@ -239,79 +239,6 @@ function doc_register_stats_upload() {
 }
 
 
-// /**
-// * Hook in and add a metabox to demonstrate repeatable grouped fields
-// */
-// function doc_register_time_schedule_metabox() {
-// $prefix = 'doc_group_';
-// **
-// * Repeatable Field Groups
-// */
-// $doc_times = new_cmb2_box( array(
-// 'id'           => $prefix . 'times_metabox',
-// 'title'        => __( 'Repeating Field Group', 'cmb2' ),
-// 'object_types' => array( 'parish' ),
-// ) );
-// $doc_times_group is the field id string, so in this case: $prefix . 'demo'
-// $doc_times_group = $doc_times->add_field( array(
-// 'id'          => $prefix . 'times',
-// 'type'        => 'group',
-// 'options'     => array(
-// 'group_title'   => __( 'Entry {#}', 'cmb2' ),
-// 'add_button'    => __( 'Add Another Entry', 'cmb2' ),
-// 'remove_button' => __( 'Remove Entry', 'cmb2' ),
-// 'sortable'      => true,
-// ),
-// ) );
-//
-// **
-// * Group fields works the same, except ids only need
-// * to be unique to the group. Prefix is not needed.
-// *
-// * The parent field's id needs to be passed as the first argument.
-// */
-// $doc_times->add_group_field( $doc_times_group, array(
-// 'name'             => __( 'Title', 'cmb2' ),
-// 'id'                => 'mass_title',
-// 'type'              => 'select',
-// 'options'           => array(
-// 'mass'           => __( 'Mass', 'cmb2' ),
-// 'adoration'      => __( 'Adoration of the Blessed Sacrament', 'cmb2' ),
-// 'reconciliation' => __( 'Reconciliation', 'cmb2' ),
-// 'holy_days'      => __( 'Holy Days', 'cmb2' ),
-// ),
-// ) );
-// $doc_times->add_group_field( $doc_times_group, array(
-// 'name'             => __( 'Day(s)', 'cmb2' ),
-// 'id'               => 'day',
-// 'type'             => 'select',
-// 'options'          => array(
-// 'monday'     => __( 'Monday', 'cmb2' ),
-// 'tuesday'    => __( 'Tuesday', 'cmb2' ),
-// 'wednesday'  => __( 'Wednesday', 'cmb2' ),
-// 'thursday'   => __( 'Thursday', 'cmb2' ),
-// 'friday'     => __( 'Friday', 'cmb2' ),
-// 'saturday'   => __( 'Saturday', 'cmb2' ),
-// 'sunday'     => __( 'Sunday', 'cmb2' ),
-// ),
-// 'repeatable' => true,
-// ) );
-// $doc_times->add_group_field( $doc_times_group, array(
-// 'name' => __( 'Test Time', 'cmb2' ),
-// 'desc' => __( 'field description (optional)', 'cmb2' ),
-// 'id'   => $prefix . 'time',
-// 'type' => 'text_time',
-// ) );
-// $doc_times->add_group_field( $doc_times_group, array(
-// 'name'        => __( '**', 'cmb2' ),
-// 'description' => __( 'Additional Information  (optional)', 'cmb2' ),
-// 'id'          => 'additional',
-// 'type'        => 'text',
-// ) );
-// }
-//
-
-
 /**
  * Gets a number of terms and displays them as options
  *
@@ -333,8 +260,8 @@ function cmb2_get_post_list( $post_type = 'post', $args = array() ) {
 			'key'       => 'doc_alias_checkbox',
 			'value'     => 'on',
 			'compare'   => 'NOT EXISTS',
-			)
-		)
+			),
+		),
 	);
 
 	$posts = get_posts( $args );
