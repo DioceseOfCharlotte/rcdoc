ff228f4786b0808ed5339b7687c3c2f3147b1b08
m-e-h <info@martyhelmick.com>
2016-05-19 17:13:27 -0400

    move header dropdown off the edge of screen

--- sidebar/header-right.php
+++ sidebar/header-right.php
@@ -5,7 +5,7 @@ if ( ! is_active_sidebar( 'header-right' ) ) {
 }
 ?>
 
-<div class="u-inline-block sidebar-header u-rel">
+<div class="u-inline-block sidebar-header u-mr2 u-rel">
 	<button href="#" class="js-dropdown dropdown-btn btn-round material-icons u-z4">account_circle</button>
 	<div class="dropdown-right u-br u-right0 u-bg-white u-shadow--3dp u-text-color">
 		<?php dynamic_sidebar( 'header-right' ); ?>
