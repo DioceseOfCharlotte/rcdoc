cf8e02123446d8afae1238013e6be22aa31a4e53
m-e-h <info@martyhelmick.com>
2016-05-22 19:53:13 -0400

    use load more on facets

--- components/posts-pagination.php
+++ components/posts-pagination.php
@@ -1,24 +1,21 @@
 <?php
-if ( is_post_type_archive( 'department' ) || is_post_type_archive( 'parish' ) || is_post_type_archive( 'school' ) ) : ?>
-	<div class="u-1of1 u-p2 u-mb2 u-content-wrap u-text-center">
-	 	<?php echo facetwp_display( 'pager' ); ?>
-	</div>
+if ( is_post_type_archive( doc_get_facet_cpts() ) ) {
+	return;
+}
 
-<?php elseif ( is_singular( 'post' ) ) : // If viewing a single post page. ?>
+if ( is_singular( 'post' ) ) : // If viewing a single post page. ?>
 
 	<div class="loop-nav u-1of1 u-text-center u-mb2">
-		<?php previous_post_link( '<div class="prev btn btn-hollow">' . esc_html__( 'Previous Post: %link', 'abraham' ) . '</div>', '%title' ); ?>
-		<?php next_post_link(     '<div class="next btn btn-hollow">' . esc_html__( 'Next Post: %link',     'abraham' ) . '</div>', '%title' ); ?>
+		<?php previous_post_link( '<div class="prev">' . esc_html__( 'Previous Post: %link', 'abraham' ) . '</div>', '%title' ); ?>
+		<?php next_post_link( '<div class="next">' . esc_html__( 'Next Post: %link', 'abraham' ) . '</div>', '%title' ); ?>
 	</div><!-- .loop-nav -->
 
-
-
-<?php elseif ( is_home() || is_archive() || is_search() ) : ?>
+<?php elseif ( is_home() || is_archive() || is_search() ) : // If viewing the blog, an archive, or search results. ?>
 
 	<?php the_posts_pagination(
 		array(
 			'prev_text' => esc_html_x( '&larr; Previous', 'posts navigation', 'abraham' ),
-			'next_text' => esc_html_x( 'Next &rarr;',     'posts navigation', 'abraham' ),
+			'next_text' => esc_html_x( 'Next &rarr;', 'posts navigation', 'abraham' ),
 		)
 	); ?>
 
