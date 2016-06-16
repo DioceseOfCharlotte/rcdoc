60e35d84bcf3b958fa734fe7b50f1ae82a8c6a89
m-e-h <info@martyhelmick.com>
2016-06-16 19:02:59 -0400

    reuse department template

--- content/cpt_archive.php
+++ content/cpt_archive.php
@@ -6,26 +6,4 @@
  */
 
 ?>
-<article <?php hybrid_attr( 'post' ); ?>>
-
-	<?php tha_entry_top(); ?>
-
-	<?php get_template_part( 'components/img', 'thumb' ); ?>
-
-	<div class="u-1of1">
-		<?php get_template_part( 'components/entry', 'header' ); ?>
-
-		<?php if ( has_excerpt() ) { ?>
-		<div <?php hybrid_attr( 'entry-summary' ); ?>>
-			<?php tha_entry_content_before(); ?>
-			<?php the_excerpt(); ?>
-			<?php tha_entry_content_after(); ?>
-		</div>
-		<?php } ?>
-	</div>
-
-		<?php get_template_part( 'components/entry', 'footer' ); ?>
-
-<?php tha_entry_bottom(); ?>
-
-</article>
+<?php get_template_part( 'content/department' ); ?>
