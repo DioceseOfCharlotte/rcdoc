767a6ed896d68f2b3a7e0baafa735960b58e2f3b
m-e-h <info@martyhelmick.com>
2016-06-27 18:15:00 -0400

    slide titles

--- components/row-slides.php
+++ components/row-slides.php
@@ -9,7 +9,7 @@ wp_enqueue_script( 'flickity' ); ?>
 
 <?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
-	<div id="post-<?php the_ID(); ?>" class="gallery-cell o-cell u-bg-white u-br u-shadow2">
+	<div id="post-<?php the_ID(); ?>" class="gallery-cell o-cell u-bg-white u-overflow-hidden u-br u-shadow2">
 
 		<header <?php hybrid_attr( 'entry-header' ); ?>>
 			<?php
@@ -21,9 +21,7 @@ wp_enqueue_script( 'flickity' ); ?>
 				'after'        => '</div>',
 			));
 			?>
-			<h3 class="card-title u-h4 u-text-center u-1of1 u-m0">
-				<a class="btn btn-hollow u-border0 u-1of1" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</h2>
+				<a class="btn u-1of1 u-bg-1 u-br0 u-text-center u-h4" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</header>
 
 	</div>
