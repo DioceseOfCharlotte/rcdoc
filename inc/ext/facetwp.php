1632e6cd0d1d815f371ac5c89be4dd5338e18844
m-e-h <info@martyhelmick.com>
2016-07-07 12:48:15 -0400

    remove unused facets

--- inc/ext/facetwp.php
+++ inc/ext/facetwp.php
@@ -28,25 +28,7 @@ function doc_has_facet() {
 }
 
 function doc_register_doc_category_facets( $facets ) {
-	$facets[] = array(
-		'label'        => 'Document Categories',
-		'name'         => 'document_categories',
-		'type'         => 'dropdown',
-		'source'       => 'tax/document_category',
-		'label_any'          => 'All Categories',
-		'orderby'         => 'display_value',
-		'hierarchical' => 'no',
-	);
 
-	$facets[] = array(
-		'label'        => 'Document Types',
-		'name'         => 'document_types',
-		'type'         => 'checkboxes',
-		'source'       => 'tax/document_type',
-		'ghosts'             => 'yes',
-		'preserve_ghosts' => 'yes',
-		'operator'     => 'or',
-	);
 
 	$facets[] = array(
 		'label'        => 'Department Agency',
@@ -92,28 +74,28 @@ function doc_register_doc_category_facets( $facets ) {
 	    'unit' => 'mi',
 	);
 
-	$facets[] = array(
-		'label'        => 'Statistics Type',
-		'name'         => 'statistics_type',
-		'type'         => 'checkboxes',
-		'source'       => 'tax/statistics_type',
-		'label_any'          => 'All Reports',
-		'orderby'         => 'display_value',
-		'hierarchical' => 'no',
-		'ghosts'       => 'yes',
-		'preserve_ghosts' => 'yes',
-		'operator'     => 'or',
-	);
-
-	$facets[] = array(
-		'label'        => 'Statistics Date',
-		'name'         => 'statistics_year',
-		'type'         => 'dropdown',
-		'source'       => 'cf/doc_stats_report_date',
-		'label_any'          => 'All Dates',
-		'orderby'         => 'display_value',
-		'hierarchical' => 'no',
-	);
+	// $facets[] = array(
+	// 	'label'        => 'Statistics Type',
+	// 	'name'         => 'statistics_type',
+	// 	'type'         => 'checkboxes',
+	// 	'source'       => 'tax/statistics_type',
+	// 	'label_any'          => 'All Reports',
+	// 	'orderby'         => 'display_value',
+	// 	'hierarchical' => 'no',
+	// 	'ghosts'       => 'yes',
+	// 	'preserve_ghosts' => 'yes',
+	// 	'operator'     => 'or',
+	// );
+	//
+	// $facets[] = array(
+	// 	'label'        => 'Statistics Date',
+	// 	'name'         => 'statistics_year',
+	// 	'type'         => 'dropdown',
+	// 	'source'       => 'cf/doc_stats_report_date',
+	// 	'label_any'          => 'All Dates',
+	// 	'orderby'         => 'display_value',
+	// 	'hierarchical' => 'no',
+	// );
 
 	return $facets;
 }
@@ -125,14 +107,9 @@ function doc_register_doc_category_facets( $facets ) {
  * @access public
  */
 function doc_display_facets() {
-	if ( is_post_type_archive( 'document' ) ) {
-		echo '<div class="u-1of3-md u-px3 u-pb0 u-br u-pt3 u-mb3 u-flex u-flex-wrap u-flex-ja u-bg-2 u-text-color u-shadow3 u-max-center">';
-		echo facetwp_display( 'facet', 'department_search' );
-		echo facetwp_display( 'facet', 'document_categories' );
-		echo facetwp_display( 'facet', 'document_types' );
-		echo '<button class="btn btn-round u-bg-frost-4 u-m0 u-m0" onclick="FWP.reset()"><span class="icon-refresh"></span></button>';
-		echo '</div>';
-	} elseif ( is_post_type_archive( 'parish' ) ) {
+	return;
+
+	if ( is_post_type_archive( 'parish' ) ) {
 		echo '<div class="u-1of1 u-px3 u-pb0 u-br u-pt3 u-mb3 u-flex u-flex-wrap u-flex-ja u-bg-2 u-text-color u-shadow3 u-max-center">';
 		echo facetwp_display( 'facet', 'proximity_search' );
 		echo facetwp_display( 'facet', 'department_search' );
@@ -152,11 +129,6 @@ function doc_display_facets() {
 		echo facetwp_display( 'facet', 'school_system' );
 		echo facetwp_display( 'facet', 'department_search' );
 		echo '<button class="btn btn-round u-bg-frost-4 u-m0 u-m0" onclick="FWP.reset()"><span class="icon-refresh"></span></button>';
-		echo '</div>';
-	} elseif ( is_post_type_archive( 'statistics_report' ) ) {
-		echo '<div class="u-1of1 u-px3 u-pb0 u-br u-pt3 u-mb3 u-flex u-flex-wrap u-flex-ja u-bg-2 u-text-color u-shadow3 u-max-center">';
-		echo facetwp_display( 'facet', 'statistics_year' );
-		echo facetwp_display( 'facet', 'statistics_type' );
 		echo '</div>';
 	}
 }
