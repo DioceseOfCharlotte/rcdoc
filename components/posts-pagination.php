1ca6213a336733bbaf2c3050b04fcea317e41c65
m-e-h <info@martyhelmick>
2016-07-19 22:33:08 -0400

    reduce hooks

--- components/posts-pagination.php
+++ components/posts-pagination.php
@@ -1,9 +1,10 @@
 <?php
 if ( is_post_type_archive( doc_get_facet_cpts() ) ) {
-	return;
-}
+	return; } ?>
 
-if ( is_singular( 'post' ) ) : ?>
+
+
+<?php if ( is_singular( 'post' ) ) : ?>
 
 	<div class="loop-nav u-1of1 u-text-center u-mb2">
 		<?php previous_post_link( '<div class="prev">' . esc_html__( 'Previous Post: %link', 'abraham' ) . '</div>', '%title' ); ?>
@@ -14,8 +15,8 @@ if ( is_singular( 'post' ) ) : ?>
 
 <div class="u-1of1 u-text-1 u-lh-1 u-mb3">
 	<?php the_posts_pagination( array(
-			'prev_text' => __( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="v-icon"><path d="M15.717 2L18 4.35 10.583 12 18 19.65 15.717 22 6 12z"/></svg>', 'abraham' ),
-			'next_text' => __( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="v-icon"><path d="M8.283 2L6 4.35 13.417 12 6 19.65 8.283 22 18 12z"/></svg>', 'abraham' )
-		) ); ?>
+		'prev_text' => __( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="v-icon"><path d="M15.717 2L18 4.35 10.583 12 18 19.65 15.717 22 6 12z"/></svg>', 'abraham' ),
+		'next_text' => __( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="v-icon"><path d="M8.283 2L6 4.35 13.417 12 6 19.65 8.283 22 18 12z"/></svg>', 'abraham' ),
+	) ); ?>
 </div>
 <?php endif; ?>
--- inc/hooks.php
+++ inc/hooks.php
@@ -6,56 +6,7 @@
  */
 
 add_action( 'tha_header_after', 'doc_article_hero' );
-add_action( 'tha_content_bottom', 'doc_dept_child_posts' );
-add_action( 'tha_content_before', 'doc_archive_desc' );
-add_action( 'tha_content_bottom', 'doc_alias_view_staff' );
-
-function doc_dept_child_posts() {
-
-	if ( ! is_singular( abe_non_hierarchy_cpts() ) && ! is_singular( abe_hierarchy_cpts() ) ) {
-		return; }
-
-	$postid = get_the_ID();
-
-	$args = array(
-	'post_parent'            => $postid,
-	'post_type'              => 'any',
-	'order'                  => 'ASC',
-	'orderby'                => 'menu_order',
-	);
-
-	$query = new WP_Query( $args );
-
-	if ( $query->have_posts() ) { ?>
-			<div class="o-cell o-grid u-m0 u-p0 u-1of1"> <?php
-			while ( $query->have_posts() ) {
-				$query->the_post();
-
-				$posted_format = get_post_format() ? get_post_format() : 'content';
-
-				tha_entry_before();
-
-				hybrid_get_content_template();
-
-				tha_entry_after();
-
-			} ?>
-		</div> <?php
-	}
-
-	wp_reset_postdata();
-
-}
-
-function doc_archive_desc() {
-	if ( ! is_paged() && $desc = get_the_archive_description() ) :
-	?>
-		<article <?php hybrid_attr( 'archive-description' ); ?>>
-			<?php echo $desc; ?>
-		</article><!-- .archive-description -->
-	<?php
-	endif;
-}
+//add_action( 'tha_content_after', 'doc_alias_view_staff' );
 
 function doc_article_hero() {
 	if ( is_front_page() ) {
@@ -63,7 +14,6 @@ function doc_article_hero() {
 
 	echo '<div id="article-hero" class="article-hero u-1of1 u-bg-center u-bg-no-repeat u-bg-cover u-tinted-image u-bg-fixed u-abs u-left0 u-right0"></div>';
 }
-
 
 function doc_alias_view_staff() {
 	global $cptarchives;
