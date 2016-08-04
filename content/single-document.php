be0df2c5924e1f3b9fffe85984b33d2f3e5b626f
m-e-h <info@martyhelmick>
2016-08-04 00:31:14 -0400

    document embed

--- content/single-document.php
+++ content/single-document.php
@@ -13,6 +13,11 @@
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php tha_entry_content_before(); ?>
 			<?php the_content(); ?>
+			<?php $doc = get_post_meta( get_the_ID(), 'dpt_document_id', true ); ?>
+			<?php $doc_link = get_attachment_link( $doc ); ?>
+
+<iframe src="https://docs.google.com/viewer?url=<?php urlencode( $doc_link ); ?>"style="height:900px;width:600px;"></iframe>
+
 			<?php tha_entry_content_after(); ?>
 		</div>
 
