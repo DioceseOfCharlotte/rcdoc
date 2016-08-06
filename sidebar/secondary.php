ba1569a47f49facbac79345aa8cc77c1b451d4aa
m-e-h <info@martyhelmick.com>
2016-08-06 19:20:19 -0400

    just style gv with css

--- sidebar/secondary.php
+++ sidebar/secondary.php
@@ -32,7 +32,7 @@ if ( ! is_active_sidebar( 'secondary' ) ) {
 
 		<?php $current_user = wp_get_current_user(); ?>
 
-			<div class="employee-section u-border u-m1 u-mb2 u-br">
+			<div class="employee-section u-bg-silver u-border u-m1 u-mb2 u-br">
 				<div class="u-border-b u-rel u-h4 u-text-1-dark u-text-display u-text-center u-mb1 u-p1">
 					<em class="u-f-plus u-opacity">Logged in as: </em><?php echo $current_user->display_name ?>
 					<a class="btn u-opacity u-abs u-p1 u-mtn1 u-left0" href="<?php echo wp_logout_url( home_url() ); ?>" title="Logout"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em"><path d="M16 17v2c0 1.105-.895 2-2 2H5c-1.105 0-2-.895-2-2V5c0-1.105.895-2 2-2h9c1.105 0 2 .895 2 2v2h-2V5H5v14h9v-2h2zm2.5-10.5l-1.414 1.414L20.172 11H10v2h10.172l-3.086 3.086L18.5 17.5 24 12l-5.5-5.5z"/></svg></a>
