4d23ed2c2fbca1e2084a58503c064387b8628392
m-e-h <info@martyhelmick.com>
2016-08-08 19:46:54 -0400

    90% of viewport on desktop for pdf size

--- content/single-document.php
+++ content/single-document.php
@@ -22,10 +22,10 @@
 
 				<div class="u-text-right new-tab-link u-1of1 u-mb2"><a class="u-link" href="<?php echo $doc_link ?>" target="_blank" rel="noopener">View this document in a new tab <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class=" u-ml1 v-icon u-f-plus"><path d="M19 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h6v2H5v12h12v-6h2zM13 3v2h4.586l-7.793 7.793 1.414 1.414L19 6.414V11h2V3h-8z"/></svg></a></div>
 
-				<object data='<?php echo $doc_link ?>#pagemode=bookmarks'
-				        type='application/pdf'
-				        width='100%'
-				        height='600px'>
+				<object class="doc-embed" data="<?php echo $doc_link ?>#pagemode=bookmarks" type="application/pdf" width="100%" height="600px">
+					<iframe src="<?php echo $doc_link ?>#pagemode=bookmarks" width="100%" height="100%" style="border: none;">
+						This browser does not support PDFs. Please download the PDF to view it:
+					</iframe>
 				</object>
 
 				<div class="u-1of1 u-text-center u-p2">
