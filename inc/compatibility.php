bc0209c71f87b049ba67a65a9f1ec4ed1f23e73f
m-e-h <info@martyhelmick.com>
2016-08-30 16:00:22 -0400

    remove menu item on admin_init

--- inc/compatibility.php
+++ inc/compatibility.php
@@ -6,7 +6,7 @@
 */
 
 add_action( 'admin_menu', 'meh_remove_menu_pages' );
-add_action( 'admin_init', 'meh_remove_jetpack_menu' );
+add_action( 'admin_init', 'meh_remove_plugins_menu' );
 add_filter( 'login_redirect', 'doc_login_redirect', 10, 3 );
 add_action( 'login_enqueue_scripts', 'doc_login_logo' );
 add_filter( 'login_headerurl', 'doc_login_logo_url' );
@@ -23,14 +23,14 @@ function meh_remove_menu_pages() {
 
 	if ( ! current_user_can( 'manage_options' ) ) {
 		remove_menu_page( 'profile.php' );
-		remove_menu_page( 'optimus-bulk-optimizer' );
 	}
 }
 
-function meh_remove_jetpack_menu() {
+function meh_remove_plugins_menu() {
 	if ( class_exists( 'Jetpack' ) && ! current_user_can( 'manage_options' ) ) {
-		remove_menu_page( 'jetpack' ); 						//Jetpack*
+		remove_menu_page( 'jetpack' );
 	}
+	remove_menu_page( 'optimus-bulk-optimizer' );
 }
 
 function doc_login_logo() {
