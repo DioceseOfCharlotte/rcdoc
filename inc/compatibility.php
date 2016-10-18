e8a1cb9142072bbc7ffabc7e663478419bc95841
m-e-h <info@martyhelmick.com>
2016-10-18 11:23:38 -0400

    remove custom login

--- inc/compatibility.php
+++ inc/compatibility.php
@@ -7,17 +7,10 @@
 
 add_action( 'admin_menu', 'meh_remove_menu_pages' );
 add_action( 'admin_init', 'meh_remove_plugins_menu' );
-add_filter( 'login_redirect', 'doc_login_redirect', 10, 3 );
-add_action( 'login_enqueue_scripts', 'doc_login_logo' );
-add_filter( 'login_headerurl', 'doc_login_logo_url' );
-add_filter( 'login_headertitle', 'doc_login_logo_url_title' );
 add_action( 'wp', 'custom_maybe_activate_user', 0 );
-
 add_filter( 'wpseo_metabox_prio', function() { return 'low'; } );
 
-function doc_login_redirect( $url, $request, $user ) {
-	return $request;
-}
+
 
 function meh_remove_menu_pages() {
 
@@ -33,26 +26,7 @@ function meh_remove_plugins_menu() {
 	}
 }
 
-function doc_login_logo() {
-	if ( ! has_custom_logo() ) { return; }
 
-	$image = wp_get_attachment_image_url( get_theme_mod( 'custom_logo' ) ); ?>
-
-		<style id="login-custom-logo">
-			#login h1 a {
-				background-image: url(<?php echo $image ?>);
-			}
-		</style>
-	<?php }
-
-function doc_login_logo_url() {
-	return home_url();
-}
-
-
-function doc_login_logo_url_title() {
-	return 'Diocese of Charlotte';
-}
 
 	/**
 	* Gravity Forms Custom Activation Template
