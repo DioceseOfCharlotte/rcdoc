7864e55463ce6759184ef2e22bf1e8920059c619
m-e-h <info@martyhelmick.com>
2016-11-16 18:23:08 -0500

    simplify home header

--- inc/ext/facetwp.php
+++ inc/ext/facetwp.php
@@ -11,6 +11,15 @@ add_action( 'tha_content_before', 'doc_display_facets' );
 //add_filter( 'facetwp_indexer_query_args', 'wpdr_facetwp_indexer_query_args' );
 add_action( 'wp_head', 'fwp_load_more', 99 );
 add_filter( 'facetwp_proximity_store_distance', '__return_true' );
+add_shortcode( 'facet_refresh', 'doc_facet_refresh' );
+
+
+// Refresh Button Shortcode
+function doc_facet_refresh() {
+	return '<button class="btn btn-round u-bg-frost-4 u-h3 u-m0 u-m0" onclick="FWP.reset()"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg></button>';
+}
+
+
 /**
  * Get the facet archive posts for adding the class to hybrid_attr_content.
  */
