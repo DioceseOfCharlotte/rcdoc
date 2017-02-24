f6c7bd5a837aa0bac1eda38a8f16ad54eb7ec444
m-e-h <info@martyhelmick.com>
2017-02-24 18:03:19 -0500

    add parish update button

--- content/single-parish.php
+++ content/single-parish.php
@@ -5,6 +5,7 @@
  * @package  RCDOC
  */
 $doc_mass = get_post_meta( get_the_ID(), 'doc_mass_schedule', true );
+$doc_pid = get_the_ID();
 ?>
 
 <article <?php hybrid_attr( 'post' ); ?>>
@@ -20,6 +21,10 @@ $doc_mass = get_post_meta( get_the_ID(), 'doc_mass_schedule', true );
 				<?php echo wpautop( $doc_mass ); ?>
 			</div>
 		<?php endif; ?>
+
+		<?php if ( current_user_can( 'edit_parishs' ) ) { ?>
+		<a class="btn" href="<?php echo esc_url( site_url( '/' ) ); ?>parish-info/?page=gravityflow-submit&id=18&doc_pid=<?php echo $doc_pid; ?>" target="blank" rel="noopener">Update Parish info</a>
+		<?php } ?>
 
 		<?php the_content(); ?>
 		<?php tha_entry_content_after(); ?>
