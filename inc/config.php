d53d5dd4362e4fa7a0f972087c88d274bd1fd6f4
m-e-h <info@martyhelmick.com>
2017-04-11 13:50:05 -0400

    remove comments

--- inc/config.php
+++ inc/config.php
@@ -5,17 +5,7 @@
  * @package  RCDOC
  */
 
-
-//add_filter( 'cleaner_gallery_defaults', 'meh_gallery_default_args' );
 add_action( 'login_footer', 'doc_login_footer' );
-
-// function meh_gallery_default_args( $defaults ) {
-// 	$defaults = array(
-// 		'size' => 'abe-hd',
-// 		'columns' => 1,
-// 	);
-// 	return $defaults;
-// }
 
 function doc_login_footer() {
 	 echo '<div class=login-links><a href="/registration/">Create an account</a></div>';
