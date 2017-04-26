f48a423405cedc69d90f38d774769fa614879385
m-e-h <info@martyhelmick.com>
2017-04-26 15:27:43 -0400

    add body class for internal IPs

--- inc/compatibility.php
+++ inc/compatibility.php
@@ -1,16 +1,27 @@
 <?php
 /**
-* For ompatiblity with third party code.
-*
-* @package  RCDOC
-*/
+ * For compatiblity with third party code.
+ *
+ * @package  RCDOC
+ */
 
+add_filter( 'body_class', 'doc_ip_body_classes' );
 add_action( 'admin_menu', 'meh_remove_menu_pages' );
-//add_action( 'admin_init', 'meh_remove_plugins_menu' );
 add_action( 'wp', 'custom_maybe_activate_user', 0 );
-add_filter( 'wpseo_metabox_prio', function() { return 'low'; } );
 
+/**
+ * Use jetpack protect IP whitelist to add bodyclass.
+ */
+function doc_ip_body_classes( $classes ) {
+	if ( is_admin() || ! function_exists( 'jetpack_protect_get_ip' ) ) {
+		return $classes; }
 
+	$ip = jetpack_protect_get_ip();
+	if ( false !== Jetpack_Protect_Module::ip_is_whitelisted( $ip ) ) {
+		$classes[] = 'doc-ip';
+	}
+	return $classes;
+}
 
 function meh_remove_menu_pages() {
 
@@ -23,15 +34,15 @@ function meh_remove_menu_pages() {
 
 
 	/**
-	* Gravity Forms Custom Activation Template
-	* http://gravitywiz.com/customizing-gravity-forms-user-registration-activation-page
-	*/
+	 * Gravity Forms Custom Activation Template
+	 * http://gravitywiz.com/customizing-gravity-forms-user-registration-activation-page
+	 */
 function custom_maybe_activate_user() {
 
 	$template_path = STYLESHEETPATH . '/content/activate.php';
 	$is_activate_page = isset( $_GET['page'] ) && $_GET['page'] == 'gf_activation';
 
-	if ( ! file_exists( $template_path ) || ! $is_activate_page  ) {
+	if ( ! file_exists( $template_path ) || ! $is_activate_page ) {
 		return; }
 
 	require_once( $template_path );
