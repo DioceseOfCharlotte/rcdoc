5546f9fbb652f16eda72264b5f21d179dbc235a2
m-e-h <info@martyhelmick.com>
2017-08-23 19:20:25 -0400

    remove the login sidebar

--- functions.php
+++ functions.php
@@ -173,9 +173,3 @@ function abe_custom_header_image( $size = 'large' ) {
 		return $bg_image;
 	}
 }
-
-add_action( 'abe_sidenav_after', 'doc_loggedin_nav' );
-
-function doc_loggedin_nav() {
-	get_template_part( 'sidebar/loggedin-sidebar' );
-}
