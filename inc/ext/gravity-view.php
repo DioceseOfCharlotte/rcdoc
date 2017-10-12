6fec9ccfeb16ca241fb69da78dabe66896a2b870
m-e-h <info@martyhelmick.com>
2017-10-12 13:56:53 -0400

    add some parish meta functions and shortcodes

--- inc/ext/gravity-view.php
+++ inc/ext/gravity-view.php
@@ -16,6 +16,10 @@ add_filter( 'gravitview_no_entries_text', 'modify_gravitview_no_entries_text', 1
 add_filter( 'gravityview/edit_entry/success', 'doc_gv_update_message', 10, 4 );
 add_filter( 'gravityview/edit_entry/cancel_link', 'doc_gv_edit_cancel', 10, 4 );
 
+add_shortcode( 'get_parish_meta', 'doc_get_parish_meta_shortcode' );
+add_shortcode( 'get_parish_address', 'doc_get_parish_address_shortcode' );
+
+
 function doc_gv_layouts() {
 
 	hybrid_register_layout('1-card-row', array(
@@ -79,4 +83,61 @@ function doc_gv_update_message( $message, $view_id, $entry, $back_link ) {
  */
 function doc_gv_edit_cancel( $back_link, $form, $entry, $view_id ) {
 	return str_replace( 'entry/' . $entry['id'] . '/', '', $back_link );
+}
+
+
+
+
+// Add Shortcode [get_parish_address id="1234"]
+function doc_get_parish_address_shortcode( $atts ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'id' => '0',
+		),
+		$atts,
+		'get_parish_address'
+	);
+
+	return doc_get_parish_address( $atts['id'] );
+}
+
+// Add Shortcode [get_parish_address id="1234"]
+function doc_get_parish_meta_shortcode( $atts ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'id'   => '0',
+			'meta' => '',
+		),
+		$atts,
+		'get_parish_meta'
+	);
+
+	return doc_get_parish_meta( $atts['id'], $atts['meta'] );
+}
+
+function doc_get_parish_address( $post_id = 0 ) {
+
+	$doc_address = '';
+
+	$doc_street = get_post_meta( $post_id, 'doc_street', true );
+	$doc_city   = get_post_meta( $post_id, 'doc_city', true );
+	$doc_state  = get_post_meta( $post_id, 'doc_state', true );
+	$doc_zip    = get_post_meta( $post_id, 'doc_zip', true );
+
+	if ( $doc_street ) {
+		$doc_address = $doc_street . '<br>' . $doc_city . ',' . $doc_state . ' ' . $doc_zip;
+	}
+
+	return $doc_address;
+}
+
+function doc_get_parish_meta( $post_id = '0', $parish_meta ) {
+
+	$doc_meta = get_post_meta( $post_id, $parish_meta, true );
+
+	return $doc_meta;
 }
